ecf367c6f05f40099dab8a9dc42851feb2015d42
Joanna K <joannakleinrm@gmail.com>
2023-08-13 22:16:38 +0200

    update

--- simple-express/routes/users.js
+++ simple-express/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const contacts = [
+  { id: '1', username: 'Felix', surname: 'Brown', email: 'felix@test.com' },
+  { id: '2', username: 'Sonya', surname: 'Redhead', email: 'sonya@test.com' },
+  { id: '3', username: 'Conan', surname: 'Barbarian', email: 'conan@test.com' },
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(contacts);
 });
+
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+  res.render('response', { title: 'Simple express app', email, password });
+});
+
 
 module.exports = router;
