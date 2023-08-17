3c2e928c94f6c48aa877a83e1503ee3a6f0bb90e
Joanna K <joannakleinrm@gmail.com>
2023-08-17 22:05:01 +0200

    jwt folder plus installs

--- simple-express/app.js
+++ simple-express/app.js
@@ -10,11 +10,12 @@ const uriDb = process.env.DB_HOST;
 const connection = mongoose.connect(uriDb, {
     useNewParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
 });
 
 connection
     .then(() => {
-        console.log("connected tp MongoDB");
+        console.log("connected to MongoDB");
     })
     .catch(e => {
         console.error(e);
@@ -25,7 +26,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use('/api', routerApi)
+// app.use('/api', routerApi)
 
 app.use((_, res, __) => {
     res.status(404).json({
@@ -34,15 +35,15 @@ app.use((_, res, __) => {
         message: 'You need to use /api route',
         data: 'Nothing found'
     })
-})
+});
 
-app.use((eer, _, res, ___) => {
-    console.error(error.stack);
+app.use((err, _, res, ___) => {
+    console.error(err.stack);
 
     res.status(500).json({
         status: 'fail',
         code: 500,
-        message: error.message,
+        message: err.message,
         data: "Internal Server Error"
     })
 });
@@ -51,7 +52,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+});
 
 
 // const createError = require('http-errors');
