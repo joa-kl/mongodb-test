bab184bd87efb67975dd710ae44e15eeba37d7f8
Joanna K <joannakleinrm@gmail.com>
2023-08-17 21:28:45 +0200

    update

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const uriDb = process.env.DB_HOST;
 
-// app.use((req, res, next) => {
-//     console.log('Nasze oprogramowanie pośredniczące');
-//     next();
-// });
+require('dotenv').config();
 
-
-
-// określamy bazową ścieżkę
-router.get("/", (req, res) => {
-    res.send("To główny router");
+const connection = mongoose.connect(uriDb, {
+    useNewParser: true,
+    useCreateIndex: true,
 });
 
-// określamy ścieżkę about
-router.get("/about", (req, res) => {
-    res.send("About");
+connection
+    .then(() => {
+        console.log("connected tp MongoDB");
+    })
+    .catch(e => {
+        console.error(e);
+    });
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api', routerApi)
+
+app.use((_, res, __) => {
+    res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'You need to use /api route',
+        data: 'Nothing found'
+    })
+})
+
+app.use((eer, _, res, ___) => {
+    console.error(error.stack);
+
+    res.status(500).json({
+        status: 'fail',
+        code: 500,
+        message: error.message,
+        data: "Internal Server Error"
+    })
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
-});
+const PORT = process.env.PORT || 3000;
 
-module.exports = router;
-
-
-// app.get('/', (req, res) => {
-//     res.send("hello world 2!")
-// });
-
-// app.get('/contact/:id', (req, res) => {
-//     res.send(`<h1>Contact page</h1> Prametr: ${req.params.id}`)
-// })
-
-// app.get('/home', (req, res) => {
-//     res.send('<h2> This is home page</h2>')
-// });
-
-
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+ 
