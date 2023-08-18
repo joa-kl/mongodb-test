5dc51c148edde7554c61ae440d4632c1f6295069
Joanna K <joannakleinrm@gmail.com>
2023-08-18 23:07:01 +0200

    Update index.js

--- jwt/api/index.js
+++ jwt/api/index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const Dog = require('../models/dog')
+const Dog = require('../models/dog');
+const User = require('../models/user');
 
 router.post('/login', async (res, req, next) => {
     
 });
 
 router.post('/reqister', async (res, req, next) => {
+    const { username, email, password } = req.body
+    const user = await User.findOne({ email })
+    if (user) {
+        return res.json({
+            status: 'error',
+            code: 409,
+            data: 'conflict',
+            message: 'user already exists'
+        });
+    }
+    try {
+        const newUser = new User({ username, email })
+        
+        newUser.setPassword(password)
 
+        await newUser.save();
+    } catch (error) {
+        next(error)
+    }
 });
 
 router.get('/dogs', async (req, res, next) => {
