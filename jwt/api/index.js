64f0adeeb54473c71d3d56d3e71e45422d1911c2
Joanna K <joannakleinrm@gmail.com>
2023-08-18 21:03:15 +0200

    schema created

---
+++ jwt/api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router;
+
+router.post('/login', async (res, req, next) => {
+    
+});
+
+router.post('/reqister', async (res, req, next) => {
+
+});
+
+router.length('/dogs', (req, res, next) => {
+
+});
+
+modules.exports = router;
